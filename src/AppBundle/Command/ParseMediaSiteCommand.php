fbf34aafb0382f5cd42571c4bf9659fa343727fb
ReenExe <ipourri@gmail.com>
2015-07-19 15:07:12 +0300

    get media page

--- src/AppBundle/Command/ParseMediaSiteCommand.php
+++ src/AppBundle/Command/ParseMediaSiteCommand.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Command;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -17,6 +18,42 @@ class ParseMediaSiteCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-       $output->writeln('<info>Execute</info>');
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        $connection->executeQuery("
+            CREATE TABLE IF NOT EXISTS `site_page_cache` (
+                `url` VARCHAR(255) PRIMARY KEY,
+                `value` TEXT
+            );
+        ");
+
+        $this->parse('http://fs.to/texts/other/');
+
+        $output->writeln('<info>Execute</info>');
+    }
+
+    private function parse($url)
+    {
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        $stm = $connection->executeQuery("
+            SELECT `value` FROM `site_page_cache`
+            WHERE `url` = :url
+        ", compact('url'));
+
+        if ($value = $stm->fetchColumn()) {
+            return $value;
+        }
+
+        $value = file_get_contents($url);
+
+        $connection->executeQuery("
+            INSERT INTO `site_page_cache` (`url`, `value`)
+            VALUES (:url, :value)
+        ", compact('url', 'value'));
+
+        return $value;
     }
 }
