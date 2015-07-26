9f5a00e6a385ec3bd1a2719606ec3711efc93a12
ReenExe <ipourri@gmail.com>
2015-07-26 22:02:52 +0300

    progress

--- src/AppBundle/Command/MediaSite/AnalyzeStructureCommand.php
+++ src/AppBundle/Command/MediaSite/AnalyzeStructureCommand.php
@@ -20,6 +20,10 @@ class AnalyzeStructureCommand extends ContainerAwareCommand
     {
         $startTime = microtime(true);
 
+        $this->createTableStructure();
+
+        $this->processing();
+
         $duration = microtime(true) - $startTime;
         $memory = memory_get_usage(true);
         $peak = memory_get_peak_usage(true);
@@ -29,5 +33,88 @@ class AnalyzeStructureCommand extends ContainerAwareCommand
             "<info>Memory:   $memory B</info>",
             "<info>Peak:     $peak B</info>",
         ]);
+    }
+
+    private function processing()
+    {
+        $source = $this->getAllSourceTag();
+
+        $map = [];
+
+        $tagMap = [];
+
+        foreach ($source as $row) {
+            $tags = json_decode($row['tags'], true);
+
+            foreach ($tags as $tag) {
+                $tagMap[$tag['name']] = true;
+
+                $map[$row['path']][$tag['name']] = $tag['list'];
+            }
+        }
+
+        $tagResultMap = array_map('array_unique', call_user_func_array('array_merge_recursive', $map));
+
+        $tagClassList = array_keys($tagResultMap);
+
+        $tagNameIdMap = array_flip($this->addTagClassList($tagClassList));
+    }
+
+    private function getAllSourceTag()
+    {
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        return $connection->fetchAll("
+            SELECT `path`, `tags` FROM `media_site_structure`
+        ");
+    }
+
+    private function createTableStructure()
+    {
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        $connection->exec("DROP TABLE IF EXISTS `media_site_tag_class`;");
+        $connection->exec("DROP TABLE IF EXISTS `media_site_tag`;");
+
+        $connection->exec("
+            CREATE TABLE IF NOT EXISTS `media_site_tag_class`(
+                `id` TINYINT PRIMARY KEY,
+                `name` VARCHAR(255),
+                UNIQUE KEY (`name`)
+            );
+        ");
+
+        $connection->exec("
+            CREATE TABLE IF NOT EXISTS `media_site_tag`(
+                `id` INT PRIMARY KEY AUTO_INCREMENT,
+                `class_id` TINYINT,
+                `name` VARCHAR(255),
+                UNIQUE KEY (`class_id`, `name`)
+            );
+        ");
+    }
+
+    private function addTagClassList(array $list)
+    {
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        $result = array_combine(range(1, count($list)), $list);
+
+        foreach ($result as $id => $tagClass) {
+
+            $connection
+                ->insert(
+                    'media_site_tag_class',
+                    [
+                        'id' => $id,
+                        'name' => $tagClass
+                    ]
+                );
+        }
+
+        return $result;
     }
 }
