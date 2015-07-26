40c6052f6e993a0a8f378bb169a9a288a11e765f
ReenExe <ipourri@gmail.com>
2015-07-26 04:59:12 +0300

    title

--- src/AppBundle/Command/CreateMediaSiteStructureCommand.php
+++ src/AppBundle/Command/CreateMediaSiteStructureCommand.php
@@ -21,6 +21,10 @@ class CreateMediaSiteStructureCommand extends ContainerAwareCommand
     {
         $startTime = microtime(true);
 
+        $this->createProcess();
+
+        $this->process(1);
+
         $duration = microtime(true) - $startTime;
         $memory = memory_get_usage(true);
         $peak = memory_get_peak_usage(true);
@@ -30,5 +34,83 @@ class CreateMediaSiteStructureCommand extends ContainerAwareCommand
             "<info>Memory:   $memory B</info>",
             "<info>Peak:     $peak B</info>",
         ]);
+    }
+
+    private function process($limit)
+    {
+        $pages = array_column($this->getPages($limit), 'path');
+
+        foreach ($pages as $path) {
+            $html = $this->getCachedPage($path);
+
+            $data = $this->getPageData($html);
+
+//            $this->updateProcess($path)
+        }
+    }
+
+    private function getPageData($html)
+    {
+        $crawler = new Crawler($html);
+
+        $data = [
+            'title' => trim($crawler->filter('.b-tab-item__title-inner h1')->text())
+        ];
+
+        return $data;
+    }
+
+    private function getPages($limit)
+    {
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        return $connection
+            ->fetchAll("
+                SELECT `path` FROM `media_site_structure_queue`
+                WHERE `process` = 0
+                LIMIT $limit;
+            ");
+    }
+
+    private function getCachedPage($path)
+    {
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        return $connection
+            ->fetchColumn("
+                SELECT `value` FROM `media_site_page_cache`
+                WHERE `path` = :path
+            " ,compact('path'));
+
+    }
+
+    private function updateProcess($path)
+    {
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        return $connection
+            ->exec("
+                UPDATE `media_site_structure_queue`
+                SET `process` = 1
+                WHERE `path` = '$path'
+            ");
+    }
+
+    private function createProcess()
+    {
+        /* @var $connection Connection */
+        $connection = $this->getContainer()->get('doctrine')->getConnection();
+
+        $connection->exec("
+            CREATE TABLE IF NOT EXISTS `media_site_structure_queue` (
+                `path` VARCHAR(255) PRIMARY KEY,
+                `process` TINYINT DEFAULT 0
+            )
+              AS
+            SELECT `path` FROM `media_site_page_queue`;
+        ");
     }
 }
