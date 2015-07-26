a171149d2e39e46eaca2cce78256652dcfe596b9
ReenExe <ipourri@gmail.com>
2015-07-26 04:39:31 +0300

    empty template

---
+++ src/AppBundle/Command/CreateMediaSiteStructureCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Doctrine\DBAL\Connection;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class CreateMediaSiteStructureCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('create:media:site:structure');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $startTime = microtime(true);
+
+        $duration = microtime(true) - $startTime;
+        $memory = memory_get_usage(true);
+        $peak = memory_get_peak_usage(true);
+
+        $output->writeln([
+            "<info>Execute:  $duration</info>",
+            "<info>Memory:   $memory B</info>",
+            "<info>Peak:     $peak B</info>",
+        ]);
+    }
+}
