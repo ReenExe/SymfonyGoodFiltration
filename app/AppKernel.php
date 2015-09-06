0940643ba37bccfdda53b6a712a961735be69470
ReenExe <ipourri@gmail.com>
2015-09-06 22:20:34 +0300

    require migrations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new FOS\ElasticaBundle\FOSElasticaBundle()
+            new FOS\ElasticaBundle\FOSElasticaBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ app/DoctrineMigrations/Version20150906191505.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150906191505 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('
+            CREATE TABLE `rr_book`(
+              `id` INT PRIMARY KEY AUTO_INCREMENT,
+              `name` VARCHAR(255),
+              `description` VARCHAR(255)
+            )
+        ');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema) {}
+}
