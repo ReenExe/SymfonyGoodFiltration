aa5baa8b7d98778365bce5c63166a10b6e266cb2
ReenExe <ipourri@gmail.com>
2015-09-13 01:00:06 +0300

    move routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,14 +3,10 @@
 namespace AppBundle\Controller;
 
 use Doctrine\DBAL\Connection;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/app/example", name="homepage")
-     */
     public function indexAction()
     {
         /* @var $connection Connection */
