6a3f96c9af110c846c072b5ae5364af67b1e9fee
ReenExe <ipourri@gmail.com>
2015-09-13 21:54:14 +0300

    include Bootstrap and jQuery

---
+++ download.vendor.assets.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js -P web/js/vendor
+wget https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js -P web/js/vendor
+
+wget https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css -P web/css/vendor
