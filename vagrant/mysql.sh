2f2a16f81eb74f92e8907b73c6cff901a5777aa0
ReenExe <ipourri@gmail.com>
2015-09-27 11:31:45 +0300

    use `Case Sensitivity`

--- vagrant/mysql.sh
+++ vagrant/mysql.sh
@@ -2,4 +2,4 @@
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password filtration"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password filtration"
 sudo aptitude install -q -y -f  mysql-server mysql-client php5-mysql
-mysql -uroot -pfiltration -e 'CREATE DATABASE `test` CHARACTER SET `utf8` COLLATE `utf8_unicode_ci`;'
+mysql -uroot -pfiltration -e 'CREATE DATABASE `test` CHARACTER SET `utf8` COLLATE `utf8_unicode_cs`;'
