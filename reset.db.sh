1710ea02a67d04bdd67cdf52fac308c15e7efd3a
ReenExe <ipourri@gmail.com>
2015-09-27 12:15:25 +0300

    progress

--- reset.db.sh
+++ reset.db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mysql -uroot -pfiltration -e 'DROP DATABASE IF EXISTS `test`;';
-mysql -uroot -pfiltration -e 'CREATE DATABASE `test` CHARACTER SET `utf8` COLLATE `utf8_unicode_ci`;'
+mysql -uroot -pfiltration -e 'CREATE DATABASE `test` CHARACTER SET `utf8` COLLATE `utf8_bin`;'
 
 mysql -uroot -pfiltration test < dump.sql
